487f3b1b28cc8d0b0a32e24eefde88a0d07224cf
andikscript <andikscript@gmail.com>
2022-08-22 18:24:15 +0700

    add domain model student for post user

---
+++ src/main/java/com/andikscript/springdataredis/model/StudentPost.java
@@ -0,0 +1,24 @@
+package com.andikscript.springdataredis.model;
+
+public class StudentPost {
+
+    private String name;
+
+    private Integer grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+}
