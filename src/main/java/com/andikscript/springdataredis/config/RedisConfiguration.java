00f42a5e0fc40db0bc2842dba8e4dfe91a11e776
andikscript <andikscript@gmail.com>
2022-08-23 06:06:05 +0700

    disable config redis because to use application.properties

--- src/main/java/com/andikscript/springdataredis/config/RedisConfiguration.java
+++ src/main/java/com/andikscript/springdataredis/config/RedisConfiguration.java
@@ -1,18 +1,18 @@
-package com.andikscript.springdataredis.config;
-
-import com.andikscript.springdataredis.model.Student;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-
-@Configuration
-public class RedisConfiguration {
-
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Student, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
-    }
-}
+//package com.andikscript.springdataredis.config;
+//
+//import com.andikscript.springdataredis.model.Student;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+//@Configuration
+//public class RedisConfiguration {
+//
+//    @Bean
+//    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<?, ?> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory);
+//        return template;
+//    }
+//}
